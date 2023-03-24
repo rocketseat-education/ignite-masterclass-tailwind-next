1a2a5aece85159994d9887cc9030d46c2b65d9d3
victordev13 <vtrcarvalho.13@gmail.com>
2023-03-23 21:14:54 -0300

    fix: remover svg que deveria ser utilizado em tema light

--- src/components/HeroPattern.tsx
+++ src/components/HeroPattern.tsx
@@ -19,13 +19,6 @@ export function HeroPattern() {
             className="absolute inset-x-0 inset-y-[-50%] h-[200%] w-full skew-y-[-18deg] fill-black/40 stroke-black/50 mix-blend-overlay dark:fill-white/2.5 dark:stroke-white/5"
           />
         </div>
-        <svg
-          viewBox="0 0 1113 440"
-          aria-hidden="true"
-          className="absolute top-0 left-1/2 ml-[-19rem] w-[69.5625rem] fill-white blur-[26px] dark:hidden"
-        >
-          <path d="M.016 439.5s-9.5-300 434-300S882.516 20 882.516 20V0h230.004v439.5H.016Z" />
-        </svg>
       </div>
     </div>
   )
